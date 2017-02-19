9b4a9ead5c60206c816fcbc55428769a8808be7a
Nicholas Tan <NicholasTan@Nicholass-MacBook-Pro.local>
2017-02-20 02:32:20 +0800

    Added background image

--- CR Chest Tracker/ViewController.swift
+++ CR Chest Tracker/ViewController.swift
@@ -13,11 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        assignBackground()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func assignBackground() {
+        let background = UIImage(named: "Background")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubview(toBack: imageView)
     }
 
 
