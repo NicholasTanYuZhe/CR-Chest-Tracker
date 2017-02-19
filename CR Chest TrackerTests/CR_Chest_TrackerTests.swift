3c09df8926b278aabc87533a5f680c9778298b99
Nicholas Tan <NicholasTan@Nicholass-MacBook-Pro.local>
2017-02-19 21:52:17 +0800

    Initial Commit

---
+++ CR Chest TrackerTests/CR_Chest_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  CR_Chest_TrackerTests.swift
+//  CR Chest TrackerTests
+//
+//  Created by Nicholas Tan on 19/02/2017.
+//  Copyright © 2017 Nicholas Tan. All rights reserved.
+//
+
+import XCTest
+@testable import CR_Chest_Tracker
+
+class CR_Chest_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
