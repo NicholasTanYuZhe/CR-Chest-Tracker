57b965d255d584db545a2bce1170834a3a2e8527
Nicholas Tan <NicholasTan@Nicholass-MacBook-Pro.local>
2017-02-22 01:43:32 +0800

    Added csvparser so that it can read the CSV file

--- CR Chest Tracker/AppDelegate.swift
+++ CR Chest Tracker/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        parseCSV()
         return true
     }
 
@@ -88,6 +89,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
+    func parseCSV() {
+        guard let csvPath = Bundle.main.path(forResource: "chestCycle", ofType: "csv") else { return }
+        do {
+            let csvData = try String(contentsOfFile: csvPath, encoding: String.Encoding.utf8)
+            let csv = csvData.csvRows()
+            
+            for row in csv {
+                print(row)
+            }
+        } catch{  
+            print(error)  
+        }
+    }
 }
 
